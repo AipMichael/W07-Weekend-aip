78c293aa23c21b47205023fa3db9621ee9126b38
AipBorges <mpbds@uic.es>
2021-11-14 11:59:33 +0100

    added login and logout thunks

--- src/redux/thunks/index.js
+++ src/redux/thunks/index.js
@@ -2,19 +2,23 @@ import { userLoginAction, userLogoutAction } from "../actions/actionCreators";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
-const apiUrl: `https://aip-netzwerk.herokuapp.com/users/login`;
+const myApi = `https://aip-netzwerk.herokuapp.com/users`;
 
-export const loginUserThunks = (user) => async (dispatch) => {
-  const response = await axios.post(`${apiUrl}login`, user);
-  console.log("hola");
-
+export const userLoginThunk = (user) => async (dispatch) => {
+  const response = await axios.post(myApi + "users/login", user);
   if (response.status === 200) {
     const token = response.data.token;
-    console.log(token);
-    const user = jwtDecode(token);
-    console.log(user);
-    /* dispatch(loginUserAction(loginUser)); */
-    /*     localStorage.setItem("tokenStorage", JSON.stringify({ token }));
-     */
+    const user = jwtDecode(token); //instaló npm i jwt-decode
+    dispatch(userLoginAction(user));
+    localStorage.setItem(
+      process.env.REACT_APP_LOCAL_STORAGE,
+      JSON.stringify({ token: token })
+    );
   }
+};
+
+export const logoutUserThunk = () => {
+  return (dispatch) => {
+    dispatch(userLogoutAction());
+  };
 };
